066199059433f2ef94e27882ca587e21413948fc
Harsh Srivastava <harshme08@gmail.com>
2026-01-04 16:56:08 +0530

    Contact Page map location updated

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -404,8 +404,7 @@ const Contact = () => {
                 viewport={{ once: true }}
               ></motion.div>
               <div className="aspect-video bg-gradient-to-br from-primary/10 to-accent/10 rounded-2xl overflow-hidden shadow-xl border border-primary/20">
-                <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30149.199972407616!2d72.82538928829403!3d19.166792131040918!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7b71de785d257%3A0x3df73dfbc35ab2cf!2sShanti%20Nagar%20CHS.!5e0!3m2!1sen!2sin!4v1767464342024!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3766.007915094161!2d72.861632!3d19.282021999999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTnCsDE2JzU1LjMiTiA3MsKwNTEnNDEuOSJF!5e0!3m2!1sen!2sin!4v1767525846779!5m2!1sen!2sin"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
